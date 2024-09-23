383c09aaaf02a12b681e446d41205ae84bd376f1
Ayca96 <aycakubra1@gmail.com>
2024-09-23 11:12:46 +0200

    "Updated app.js and app.ts files with new variable declarations, array initialization, and method calls."

--- class-notes/s01/app.js
+++ class-notes/s01/app.js
@@ -1,4 +1,18 @@
 console.log("Cohort DE09");
 var b = 5;
 var c = 2;
-b = "5";
+// b ="5"; // hata veriyor
+var e = "Hello";
+var f = "World";
+var g;
+var h; // any atamasi yapar automatisch
+var x, y, z; // tek satirda birden fazla degiskene type belirtemiyoruz, ilkinden sonrakileri any olarak kabul ediyor.
+x = "Merhaba";
+y = 5;
+// Arrays
+var nums = [1, 2, "Hi"];
+nums.push("Merhaba");
+nums.push(5);
+nums.push(false);
+console.log(nums);
+nums.forEach(function (item) { return item.toLowerCase(); });
--- class-notes/s01/app.ts
+++ class-notes/s01/app.ts
@@ -4,4 +4,34 @@ console.log("Cohort DE09");
 let b = 5;
 let c = 2;
 
-b ="5";
+// b ="5"; // hata veriyor
+
+let e : string= "Hello"
+let f = "World"
+
+let g : boolean
+let h; // any atamasi yapar automatisch
+
+let x,y,z :string // tek satirda birden fazla degiskene type belirtemiyoruz, ilkinden sonrakileri any olarak kabul ediyor.
+
+x="Merhaba";
+y=5
+
+// Arrays
+
+// let nums : number [] = [1,2, "Hi"]
+
+// nums.push("Merhaba")
+// nums.push(5);
+// nums.push(false)
+
+// console.log(nums);
+
+// nums.forEach(item=>item.toLowerCase())
+
+let usernames : string [] = ["Kemal", "Mustafa"]
+usernames.push("Ayse")
+// usernames.push(5) // Argument of type "number" is not assignable to parameter of type "string"
+
+let surnames : string [] = [];
+surnames.push("Harold")
