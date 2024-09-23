87eafd14aa9cd17e6e4b4187c9ccd10107155a2c
Ayca96 <aycakubra1@gmail.com>
2024-09-23 11:49:55 +0200

    "Updated app.js and app.ts files with new code examples for arrays, tuples, and enums in TypeScript."

--- class-notes/s01/app.js
+++ class-notes/s01/app.js
@@ -10,9 +10,27 @@ var x, y, z; // tek satirda birden fazla degiskene type belirtemiyoruz, ilkinden
 x = "Merhaba";
 y = 5;
 // Arrays
-var nums = [1, 2, "Hi"];
-nums.push("Merhaba");
-nums.push(5);
-nums.push(false);
-console.log(nums);
-nums.forEach(function (item) { return item.toLowerCase(); });
+// let nums : number [] = [1,2, "Hi"]
+// nums.push("Merhaba")
+// nums.push(5);
+// nums.push(false)
+// console.log(nums);
+// nums.forEach(item=>item.toLowerCase())
+var usernames = ["Kemal", "Mustafa"];
+usernames.push("Ayse");
+// usernames.push(5) // Argument of type "number" is not assignable to parameter of type "string"
+var surnames = [];
+surnames.push("Harold");
+surnames.push("25");
+// type declaration 2.yol
+// let nums2 : Array <number> = [5,6]
+// console.log(surnames);
+//* Tuples
+var myTuple = [5, "merhaba", false];
+console.log(myTuple);
+var myTuple2;
+myTuple2 = [6, "World"]; // eksik eleman mevcut
+console.log(myTuple2);
+var currentUser = 3 /* Role.DbAdmin */;
+currentUser = 4 /* Role.Tester */;
+console.log(currentUser);
--- class-notes/s01/app.ts
+++ class-notes/s01/app.ts
@@ -39,7 +39,66 @@ surnames.push("25")
 
 // type declaration 2.yol
 
-let nums2 : Array <number> = [5,6]
+// let nums2 : Array <number> = [5,6]
 
-console.log(surnames);
+// console.log(surnames);
 
+//* Tuples
+
+let myTuple :[number,string,boolean] = [5, "merhaba", false]
+console.log(myTuple);
+
+let myTuple2 : [number,string,boolean];
+myTuple2 = [6,"World"] // eksik eleman mevcut
+console.log(myTuple2);
+
+
+// Tuples of Array
+
+// let myTuple3 : [number,string] []
+// myTuple3 = [[5, "5"] , [6, "6"]]
+// myTuple3.push([7,"7"])
+// myTuple3.push(false) // ts error
+// console.log(myTuple3);
+
+
+//? Enum  ( numaralandirma)
+
+// const enum Role {
+//   User, // User = 0
+//   Admin, // Admin = 1
+//   Tester // Tester = 2
+
+// }
+
+// let currentUser : Role = Role.Admin
+// console.log(currentUser);
+
+const enum Role {
+  User, // User = 0
+  Admin, // Admin = 1
+  DbAdmin = Admin * 3,
+  Tester // Tester = 2
+
+}
+
+let currentUser : Role = Role.DbAdmin
+currentUser =  Role.Tester
+console.log(currentUser);
+
+enum StatusCodes {
+  NotFound = 404,
+  Created = 201,
+  Accepted = 202,
+  BadRequest = 500,
+  NoContent = 204,
+  Success= 200,
+}
+
+const response: StatusCodes = StatusCodes.Success
+
+enum Sizes {
+  Small ="S",
+  Medium = "M",
+  Large = "L"
+}
