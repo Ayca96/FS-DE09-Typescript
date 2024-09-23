a1ede6ea532a4330bb96cf5f986f1181e0c30ab6
Ayca96 <aycakubra1@gmail.com>
2024-09-23 12:16:45 +0200

    "Added enum types for StatusCodes and Sizes, and modified existing code to use these enums; commented out various lines of code in app.ts"

--- class-notes/s01/app.js
+++ class-notes/s01/app.js
@@ -34,3 +34,24 @@ console.log(myTuple2);
 var currentUser = 3 /* Role.DbAdmin */;
 currentUser = 4 /* Role.Tester */;
 console.log(currentUser);
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["Created"] = 201] = "Created";
+    StatusCodes[StatusCodes["Accepted"] = 202] = "Accepted";
+    StatusCodes[StatusCodes["BadRequest"] = 500] = "BadRequest";
+    StatusCodes[StatusCodes["NoContent"] = 204] = "NoContent";
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+})(StatusCodes || (StatusCodes = {}));
+var response = StatusCodes.Success;
+var Sizes;
+(function (Sizes) {
+    Sizes["Small"] = "S";
+    Sizes["Medium"] = "M";
+    Sizes["Large"] = "L";
+    Sizes[Sizes["XLarge"] = 100] = "XLarge";
+    Sizes[Sizes["XXLarge"] = 101] = "XXLarge";
+})(Sizes || (Sizes = {}));
+Sizes.Medium;
+Sizes["Medium"];
+Sizes[101]; // XXLarge
--- class-notes/s01/app.ts
+++ class-notes/s01/app.ts
@@ -1,21 +1,21 @@
 console.log("Cohort DE09");
 
 
-let b = 5;
-let c = 2;
+// let b = 5;
+// let c = 2;
 
-// b ="5"; // hata veriyor
+// // b ="5"; // hata veriyor
 
-let e : string= "Hello"
-let f = "World"
+// let e : string= "Hello"
+// let f = "World"
 
-let g : boolean
-let h; // any atamasi yapar automatisch
+// let g : boolean
+// let h; // any atamasi yapar automatisch
 
-let x,y,z :string // tek satirda birden fazla degiskene type belirtemiyoruz, ilkinden sonrakileri any olarak kabul ediyor.
+// let x,y,z :string // tek satirda birden fazla degiskene type belirtemiyoruz, ilkinden sonrakileri any olarak kabul ediyor.
 
-x="Merhaba";
-y=5
+// x="Merhaba";
+// y=5
 
 // Arrays
 
@@ -29,13 +29,13 @@ y=5
 
 // nums.forEach(item=>item.toLowerCase())
 
-let usernames : string [] = ["Kemal", "Mustafa"]
-usernames.push("Ayse")
-// usernames.push(5) // Argument of type "number" is not assignable to parameter of type "string"
+// let usernames : string [] = ["Kemal", "Mustafa"]
+// usernames.push("Ayse")
+// // usernames.push(5) // Argument of type "number" is not assignable to parameter of type "string"
 
-let surnames : string [] = [];
-surnames.push("Harold")
-surnames.push("25")
+// let surnames : string [] = [];
+// surnames.push("Harold")
+// surnames.push("25")
 
 // type declaration 2.yol
 
@@ -45,12 +45,12 @@ surnames.push("25")
 
 //* Tuples
 
-let myTuple :[number,string,boolean] = [5, "merhaba", false]
-console.log(myTuple);
+// let myTuple :[number,string,boolean] = [5, "merhaba", false]
+// console.log(myTuple);
 
-let myTuple2 : [number,string,boolean];
-myTuple2 = [6,"World"] // eksik eleman mevcut
-console.log(myTuple2);
+// let myTuple2 : [number,string,boolean];
+// myTuple2 = [6,"World"] // eksik eleman mevcut
+// console.log(myTuple2);
 
 
 // Tuples of Array
@@ -74,31 +74,86 @@ console.log(myTuple2);
 // let currentUser : Role = Role.Admin
 // console.log(currentUser);
 
-const enum Role {
-  User, // User = 0
-  Admin, // Admin = 1
-  DbAdmin = Admin * 3,
-  Tester // Tester = 2
+// const enum Role {
+//   User, // User = 0
+//   Admin, // Admin = 1
+//   DbAdmin = Admin * 3,
+//   Tester // Tester = 2
 
-}
+// }
 
-let currentUser : Role = Role.DbAdmin
-currentUser =  Role.Tester
-console.log(currentUser);
+// let currentUser : Role = Role.DbAdmin
+// currentUser =  Role.Tester
+// console.log(currentUser);
 
-enum StatusCodes {
-  NotFound = 404,
-  Created = 201,
-  Accepted = 202,
-  BadRequest = 500,
-  NoContent = 204,
-  Success= 200,
-}
+// enum StatusCodes {
+//   NotFound = 404,
+//   Created = 201,
+//   Accepted = 202,
+//   BadRequest = 500,
+//   NoContent = 204,
+//   Success= 200,
+// }
 
-const response: StatusCodes = StatusCodes.Success
+// const response: StatusCodes = StatusCodes.Success
 
-enum Sizes {
-  Small ="S",
-  Medium = "M",
-  Large = "L"
-}
+// enum Sizes {
+//   Small ="S",
+//   Medium = "M",
+//   Large = "L",
+//   XLarge = 100,
+//   XXLarge
+// }
+
+// Sizes.Medium
+// Sizes["Medium"]
+// Sizes[101] // XXLarge
+
+//? any (herhangi) kacis yolu gibi, iyi bir typescript yazmak istiyorsak pek kullanmamak lazim API ile calisirken bazen veri tipi belirsiz olabiliyor test etme sansi yoksa o zaman kullanilabiliyor.
+
+// let h: any = 1
+// h = " Hello"
+
+// let k : boolean = h
+
+
+// //? unknown
+// let j :unknown = 5;
+
+// j = "5";
+
+// let m : string = j // unknown & any farki
+
+// let n : string = j as string // bu sekilde yapilabilir.  type assertions bu yöntem de mümkün mertebe kullanilmamasi gerekn bir yöntem
+
+//? voidtype
+
+function warnUser() :void {
+  console.log("Warning");
+  
+} // return kullanmayan bir fonksiyon. Return yazinca hata atiyor
+
+// function warnUser2 () : void {
+//   console.log("Warning")
+//   return "warn"
+  
+// } //error atiyor
+
+// function warnUser3 () : string {
+//   console.log("Warning")
+//   return "warn"
+  
+// } // return yazarsak ne döndürüyorsa o type yazdirmamiz lazim.
+
+
+
+// let myfunc = warnUser ()
+// console.log(myfunc);
+
+
+// never type
+
+// function throwError (): never {
+//   throw new Error ("error")
+  
+// }
