3c87f8d5d2627b31d1eaedfb570aa8b5812a3724
Ayca96 <aycakubra1@gmail.com>
2024-09-24 11:56:46 +0200

    "Added task interface, enum, and event listener to app.ts; refactored app.js to match"

--- class-notes/s02/root/app.ts
+++ class-notes/s02/root/app.ts
@@ -3,3 +3,29 @@ const input = document.getElementById("todoInput") as HTMLInputElement;
 const addBtn = document.getElementById("addToDoBtn") as HTMLButtonElement;
 
 const list = document.getElementById("todoList") as HTMLUListElement;
+
+// {
+//   task:"asda",
+//   status:0-1,
+
+// }
+
+const enum Status {
+  Active,  // 0
+  Completed, // 1
+}
+interface ITask{
+
+  task:string;
+  status:Status
+}
+
+let tasks: ITask[] = [];
+
+addBtn.addEventListener("click", ()=>{
+  const newTask = input.value.trim() // input string oldugu icin string methodlari cikti ve otomatik newTask te string sayiliyor.
+  if(newTask){
+    tasks.push({task:newTask, status:Status.Active})
+  }
+})
+
--- class-notes/s02/src/app.js
+++ class-notes/s02/src/app.js
@@ -1,3 +1,10 @@
 const input = document.getElementById("todoInput");
 const addBtn = document.getElementById("addToDoBtn");
 const list = document.getElementById("todoList");
+let tasks = [];
+addBtn.addEventListener("click", () => {
+    const newTask = input.value.trim(); // input string oldugu icin string methodlari cikti ve otomatik newTask te string sayiliyor.
+    if (newTask) {
+        tasks.push({ task: newTask, status: 0 /* Status.Active */ });
+    }
+});
