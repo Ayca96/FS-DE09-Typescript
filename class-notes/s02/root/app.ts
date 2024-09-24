6fb63f4bf713326fca815074c144bb1652ce7f4f
Ayca96 <aycakubra1@gmail.com>
2024-09-24 12:10:53 +0200

    "Added updateList function to update todo list UI on task addition"

--- class-notes/s02/root/app.ts
+++ class-notes/s02/root/app.ts
@@ -22,11 +22,27 @@ interface ITask{
 
 let tasks: ITask[] = [];
 
+
+const updateList = () =>{
+
+list.innerHTML = "";
+tasks.forEach((item, index) =>{
+const li = document.createElement("li");
+li.textContent = item.task;
+li.classList.add(item.status === Status.Completed ? "completed" : "active")
+list.appendChild(li)
+
+})
+
+}
+
+
 addBtn.addEventListener("click", ()=>{
   const newTask = input.value.trim() // input string oldugu icin string methodlari cikti ve otomatik newTask te string sayiliyor.
   if(newTask){
     tasks.push({task:newTask, status:Status.Active})
     console.log(tasks);
+    updateList()
     input.value ="";
     
   }
--- class-notes/s02/src/app.js
+++ class-notes/s02/src/app.js
@@ -2,11 +2,21 @@ const input = document.getElementById("todoInput");
 const addBtn = document.getElementById("addTodoBtn");
 const list = document.getElementById("todoList");
 let tasks = [];
+const updateList = () => {
+    list.innerHTML = "";
+    tasks.forEach((item, index) => {
+        const li = document.createElement("li");
+        li.textContent = item.task;
+        li.classList.add(item.status === 1 /* Status.Completed */ ? "completed" : "active");
+        list.appendChild(li);
+    });
+};
 addBtn.addEventListener("click", () => {
     const newTask = input.value.trim(); // input string oldugu icin string methodlari cikti ve otomatik newTask te string sayiliyor.
     if (newTask) {
         tasks.push({ task: newTask, status: 0 /* Status.Active */ });
         console.log(tasks);
+        updateList();
         input.value = "";
     }
 });
