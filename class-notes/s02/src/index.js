17c0690a1769a32a66666a7520b645a064076f8a
Ayca96 <aycakubra1@gmail.com>
2024-09-24 11:00:10 +0200

    "Added type annotations and examples for arrays and type inference in index.ts and index.js files"

--- class-notes/s02/src/index.js
+++ class-notes/s02/src/index.js
@@ -37,3 +37,33 @@ const adminUser = {
     getName: () => console.log("admin"),
 };
 console.log(adminUser);
+// const users = [] // any[]
+// users.push(5)
+// users.push({username:"s"})
+const users = [];
+// users.push(5)
+// users.push("5")
+// users.push({username:"as"}) // error 
+users.push({
+    username: "Ayca",
+    firstName: "Ayca",
+    age: 28,
+    getName: () => "as"
+});
+//! Type Inference 
+//Tip İnferansı (Type Inference), TypeScript'te derleyicinin, bir değişkenin veya bir ifadenin türünü otomatik olarak tahmin edebilmesidir. Yani, bir veri türünü açıkça belirtmesen bile, TypeScript o değerin türünü belirleyebilir.
+let myName = "Ayca";
+// myName = 5 
+let arr = []; // any [] bos arrayi default olarak any array kabul ediyor.
+let myArr = [
+    {
+        username: "ayca",
+        department: " IT",
+        salary: 5000,
+    }
+];
+// myArr.push(5)
+// myArr.push({name:"asda"})
+myArr.push({ username: "asda", department: "IT", salary: 6000 });
+const myArr2 = [...myArr, ...users];
+// myArr2.push(5)
